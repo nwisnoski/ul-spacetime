1d40f44435265cafad58c95ead83b43f88fd7aef
Nathan Wisnoski <nathan.wisnoski@msstate.edu>
2026-05-11 17:09:12 -0500

    started using linter, cleaning up code structure

--- bin/mothur_tools.R
+++ bin/mothur_tools.R
@@ -7,7 +7,7 @@
 # Written by: Mario Muscarella                                                 #
 #                                                                              #
 # Last update: 2019/01/04 by N. Wisnoski to drop reshape and work with stringr
-#  
+#
 #                                                                              #
 ################################################################################
 #                                                                              #
@@ -27,24 +27,26 @@ library("stringr")
 library("data.table")
 
 # Import OTU Site-by-Species Matrix
-read.otu <- function(shared = " ", cutoff = "0.03"){
-  matrix <- fread(shared, header=T, fill=TRUE, sep="\t")
+read.otu <- function(shared = " ", cutoff = "0.03") {
+  matrix <- fread(shared, header = T, fill = TRUE, sep = "\t")
   matrix.cutoff <- subset(matrix, matrix$label == cutoff)
-  matrix.out    <- as.matrix(matrix.cutoff[1:dim(matrix.cutoff)[1],
-                                           4:(3+mean(matrix.cutoff$numOtus))])
+  matrix.out <- as.matrix(matrix.cutoff[
+    1:dim(matrix.cutoff)[1],
+    4:(3 + mean(matrix.cutoff$numOtus))
+  ])
   row.names(matrix.out) <- matrix.cutoff$Group
   return(matrix.out)
-  }
+}
 
 # Import Taxonomy Information
-read.tax <- function(taxonomy = " ", format = "rdp"){
-  tax_raw <- read.delim(taxonomy)                 # load genus-level data
-  if (format == "rdp"){
-    tax <- str_split_fixed(tax_raw[,3], pattern = "\\;", 6)
-    colnames(tax) <- c("Domain","Phylum","Class","Order","Family","Genus")
-    tax <- cbind.data.frame(OTU = tax_raw[,1], tax)
-    for (i in 2:7){
-      tax[,i] <- gsub("\\(.*$", "", tax[,i])
+read.tax <- function(taxonomy = " ", format = "rdp") {
+  tax_raw <- read.delim(taxonomy) # load genus-level data
+  if (format == "rdp") {
+    tax <- str_split_fixed(tax_raw[, 3], pattern = "\\;", 6)
+    colnames(tax) <- c("Domain", "Phylum", "Class", "Order", "Family", "Genus")
+    tax <- cbind.data.frame(OTU = tax_raw[, 1], tax)
+    for (i in 2:7) {
+      tax[, i] <- gsub("\\(.*$", "", tax[, i])
     }
   } else {
     stop("This funciton currently only works for RDP taxonomy")
